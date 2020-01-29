23ba562b5679ca96f7d39fe19dce09b7054ce802
sunnySun-gituser <57857509+sunnySun-gituser@users.noreply.github.com>
2020-01-29 15:46:40 -0800

    Updates

---
+++ precache-manifest.6ae596b92a9b6f7492fd021f15f4fdbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002f75c5346a06e5db0b77990e512e9b",
+    "url": "/recipesApp/index.html"
+  },
+  {
+    "revision": "7b7eb321fc746feba454",
+    "url": "/recipesApp/static/css/main.7482957e.chunk.css"
+  },
+  {
+    "revision": "81c0d3d971ba2a27b9ac",
+    "url": "/recipesApp/static/js/2.39d27fca.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/recipesApp/static/js/2.39d27fca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b7eb321fc746feba454",
+    "url": "/recipesApp/static/js/main.a9ab868c.chunk.js"
+  },
+  {
+    "revision": "c64c121191641adb8362",
+    "url": "/recipesApp/static/js/runtime-main.2641e93a.js"
+  }
+]);
